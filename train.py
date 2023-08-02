11b8b5c6fd92304314aff1f6ec7de0597bdaa3b0
Charles Wong <charleswonghm@gmail.com>
2023-08-02 16:07:16 -0400

    Optimize validation

--- train.py
+++ train.py
@@ -16,6 +16,8 @@ from cfg import MODEL_CHECKPOINT, SAVE_FILE, MAX_INPUT_LENGTH, MAX_OUTPUT_LENGTH
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 MODEL = None
 TOKENIZER = None
 
@@ -23,7 +25,7 @@ TOKENIZER = None
 #           HYPERPARAMETERS         #
 #####################################
 BATCH_SIZE = 8
-NUM_EPOCHS = 5
+NUM_EPOCHS = 8
 LEARNING_RATE = 5e-5
 
 
@@ -68,13 +70,13 @@ if __name__ == '__main__':
 
     train_dl = DataLoader(preprocess_data(train_data), batch_size=BATCH_SIZE,
                           shuffle=True, num_workers=2, pin_memory=gpu)
-    val_dl = DataLoader(preprocess_data(val_data), batch_size=BATCH_SIZE, num_workers=2,
+    val_dl = DataLoader(preprocess_data(val_data), batch_size=BATCH_SIZE * 2, num_workers=2,
                         pin_memory=gpu)
 
     accumulation_steps = 8
     num_training_steps = NUM_EPOCHS * len(train_dl)
     num_optimizer_steps = ceil(num_training_steps / accumulation_steps)
-    num_warmup_steps = num_optimizer_steps // 100
+    num_warmup_steps = num_optimizer_steps // 10
     num_winddown = num_optimizer_steps - num_warmup_steps
     optimizer = AdamW(MODEL.parameters(), lr=LEARNING_RATE)
     lr_scheduler = get_scheduler(name="linear", optimizer=optimizer,
@@ -97,7 +99,7 @@ if __name__ == '__main__':
     with tqdm(total=num_training_steps, desc='Training', unit='epoch',
               bar_format=barfmt % 0, position=0, dynamic_ncols=True) as pbar:
         for epoch in trange(1, NUM_EPOCHS + 1):
-            with tqdm(train_dl, desc=f'Epoch {epoch}', leave=False, unit='batch',
+            with tqdm(total=len(train_dl), desc=f'Epoch {epoch}', leave=False, unit='batch',
                       position=1, dynamic_ncols=True) as it:
 
                 # train
@@ -118,32 +120,31 @@ if __name__ == '__main__':
 
                         lr_scheduler.step()
                         optimizer.zero_grad()
-                    it.set_postfix(loss=loss.item())
+                    it.set_postfix(loss=loss.item() * accumulation_steps)
                     it.update()
                     pbar.update()
 
             # eval
             MODEL.eval()
-            total_rouge_score = 0
-            examples = 0
-            for batch in val_dl:
-                batch = {k: v.to(device) for k, v in batch.items()}
-                with torch.no_grad():
-                    out = MODEL.generate(input_ids=batch["input_ids"], num_beams=2, max_length=MAX_OUTPUT_LENGTH)
-                    decoded_out = TOKENIZER.batch_decode(out, skip_special_tokens=True,
-                                                         clean_up_tokenization_spaces=False)
+            rouge_scores = []
+            with tqdm(total=len(val_dl), desc=f'Validating Epoch {epoch}', leave=False, unit='batch',
+                      position=1, dynamic_ncols=True) as it:
+                for batch in val_dl:
+                    batch = {k: v.to(device) for k, v in batch.items()}
+                    with torch.no_grad():
+                        out = MODEL.generate(input_ids=batch["input_ids"], num_beams=2, max_length=MAX_OUTPUT_LENGTH)
+                        decoded_out = TOKENIZER.batch_decode(out, skip_special_tokens=True,
+                                                             clean_up_tokenization_spaces=False)
 
-                    decoded_labels = TOKENIZER.batch_decode(batch["labels"], skip_special_tokens=True,
-                                                            clean_up_tokenization_spaces=False)
+                        decoded_labels = TOKENIZER.batch_decode(batch["labels"], skip_special_tokens=True,
+                                                                clean_up_tokenization_spaces=False)
 
-                    total_score = 0
-                    for i in range(len(decoded_out)):
-                        score = scorer.score(decoded_out[i], decoded_labels[i])
+                        rouge_scores.extend(scorer.score(decoded_out[i], decoded_labels[i])['rougeL'].fmeasure
+                                            for i in range(len(decoded_out)))
 
-                        total_rouge_score += score['rougeL'].fmeasure
-                        examples += 1
+                        it.update()
 
-            mean_rouge_score = total_rouge_score / examples
+            mean_rouge_score = np.mean(rouge_scores)
             tqdm.write(f"Mean ROUGE fmesaure after epoch {epoch}: {mean_rouge_score}")
             validation_score.append(mean_rouge_score)
 
@@ -154,7 +155,7 @@ if __name__ == '__main__':
 
 
     # plot results
-    fig, ((ax1, ax2), (ax3, _)) = plt.subplots(2, 2)
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12.8, 9.6))
     save = False
 
     training_steps = [x for x in range(1, num_training_steps + 1)]
@@ -169,14 +170,27 @@ if __name__ == '__main__':
 
     epochs = [x for x in range(1, NUM_EPOCHS + 1)]
 
-    ax2.plot(training_steps, loss.cpu(), "-")
-    ax2.set_xlabel("Epoch")
-    ax2.set_ylabel("Validation Score (Mean ROUGE-L F-measure)")
+    ax3.plot(epochs, validation_score, "-")
+    ax3.set_xlabel("Epoch")
+    ax3.set_ylabel("Validation Score (Mean ROUGE-L F-measure)")
 
     plt.suptitle("Ketchup Model Training Metrics")
     plt.tight_layout()
+
+    fig.delaxes(ax4)
 
     if save:
         plt.savefig("ketchup_training_metrics.png")
 
     plt.show()
+
+
+    # Mean ROUGE fmesaure after epoch 1: 0.3807100916739339
+    # Mean ROUGE fmesaure after epoch 2: 0.39435794575548017
+    # Mean ROUGE fmesaure after epoch 3: 0.40296773950530385
+    # Mean ROUGE fmesaure after epoch 4: 0.41401589685583345
+    # Mean ROUGE fmesaure after epoch 5: 0.41093657586168725
+    # Mean ROUGE fmesaure after epoch 6: 0.4176324116119637
+    # Mean ROUGE fmesaure after epoch 7: 0.41798957576730356
+    # Mean ROUGE fmesaure after epoch 8: 0.4166328363868793
+
